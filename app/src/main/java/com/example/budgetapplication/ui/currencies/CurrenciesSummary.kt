961ba42387827a515ed6aef4e0fc2fb32d594558
Andres Cardenas <afcarzero1@gmail.com>
2023-08-09 17:19:26 +0200

    Create list

--- app/src/main/java/com/example/budgetapplication/ui/currencies/CurrenciesSummary.kt
+++ app/src/main/java/com/example/budgetapplication/ui/currencies/CurrenciesSummary.kt
@@ -2,22 +2,24 @@ package com.example.budgetapplication.ui.currencies
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.budgetapplication.ui.AppViewModelProvider
 import androidx.compose.runtime.getValue
-import com.example.budgetapplication.data.currencies.Currency
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.budgetapplication.data.currencies.Currency
+import com.example.budgetapplication.ui.AppViewModelProvider
 
 
 @Composable
@@ -76,32 +78,87 @@ fun CurrencyItem(
 ) {
 
     Card(
-        modifier = modifier,
+        modifier = modifier.padding(
+            horizontal = 16.dp,
+            vertical = 4.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
 
-        Row(modifier = modifier) {
+            Text(
+                text = currency.name + "/USD",
+                modifier = Modifier.padding(
+                    start = 20.dp,
+                    end = 20.dp
+                ),
+            )
 
-            Column(modifier = modifier) {
-                //todo: change to currency icon
-                Icon(
-                    imageVector = Icons.Default.Info,
-                    contentDescription = "Info",
-                    modifier = modifier
-                )
-            }
-            Column(modifier = modifier) {
-                Text(
-                    text = currency.name,
-                    modifier = modifier
-                )
+            Text(
+                text = currency.updatedTime,
+                modifier = Modifier.padding(
+                    start = 20.dp,
+                    end = 20.dp
+                ),
+            )
 
-                Text(
-                    text = currency.value.toString(),
-                    modifier = modifier
-                )
-            }
+            Text(text = currency.value.toString())
 
         }
     }
+}
+
+@Preview
+@Composable
+fun CurrenciesItemPreview() {
+    val currency = Currency(
+        name = "USD",
+        id = 0,
+        value = 1.0f,
+        updatedTime = "2021-10-10 10:10:10"
+    )
+
+    CurrencyItem(
+        currency = currency,
+        modifier = Modifier.fillMaxSize()
+    )
+}
+
+
+@Preview
+@Composable
+fun CurrenciesListPreview() {
+    val currencies = listOf(
+        Currency(
+            name = "USD",
+            id = 0,
+            value = 1.0f,
+            updatedTime = "2021-10-10 10:10:10"
+        ),
+        Currency(
+            name = "EUR",
+            id = 0,
+            value = 1.1f,
+            updatedTime = "2021-10-10 10:10:10"
+        ),
+        Currency(
+            name = "SEK",
+            id = 0,
+            value = 0.1f,
+            updatedTime = "2021-10-10 10:10:10"
+        )
+    )
+
+    CurrenciesList(
+        currenciesList = currencies,
+        modifier = Modifier.fillMaxSize()
+    )
+
 }
