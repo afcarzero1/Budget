7c27b7f8e9ea6b3cc92040f3d5da05080ab6eab0
Andres Cardenas <afcarzero1@gmail.com>
2023-08-13 12:25:26 +0200

    Add transactions

---
+++ app/src/main/java/com/example/budgetapplication/data/transactions/TransactionDao.kt
@@ -0,0 +1,27 @@
+package com.example.budgetapplication.data.transactions
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TransactionDao {
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insert(transactionRecord: TransactionRecord)
+
+    @Update
+    suspend fun update(transactionRecord: TransactionRecord)
+
+    @Delete
+    suspend fun delete(transactionRecord: TransactionRecord)
+
+    @Query("SELECT * from transactions WHERE id = :id")
+    fun getTransactionStream(id: Int): Flow<TransactionRecord>
+
+    @Query("SELECT * from transactions ORDER BY date DESC")
+    fun getAllTransactionsStream(): Flow<List<TransactionRecord>>
+}
---
+++ app/src/main/java/com/example/budgetapplication/data/transactions/TransactionRecord.kt
@@ -0,0 +1,40 @@
+package com.example.budgetapplication.data.transactions
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.example.budgetapplication.data.DateConverter
+import com.example.budgetapplication.data.accounts.Account
+import com.example.budgetapplication.data.categories.Category
+import java.time.LocalDateTime
+
+@Entity(
+    tableName = "transactions",
+    foreignKeys = [
+        ForeignKey(
+            entity = Account::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("accountId"),
+            onDelete = ForeignKey.RESTRICT
+        ),
+        ForeignKey(
+            entity = Category::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("categoryId"),
+            onDelete = ForeignKey.RESTRICT
+        )
+    ]
+)
+data class TransactionRecord (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val type: String,
+    val accountId: Int,
+    val categoryId: Int,
+    val amount: Float,
+    @TypeConverters(DateConverter::class)
+    val date: LocalDateTime,
+)
